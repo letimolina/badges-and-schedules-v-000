556950121cac1b931ed6bc7f826fa1a004377ef5
Leticia Molina <letimolina@gmail.com>
2018-05-20 19:39:49 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,17 +1,29 @@
 attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
 def badge_maker(name)
-  "Hello, my name is #{name}."
+ "Hello, my name is #{name}."
 end
 
 def batch_badge_creator(array)
- 
+  badge_messages = []
+  array.each do |name|
+    badge_messages.push(badge_maker(name))
+  end
+ badge_messages
 end
 
 def assign_rooms(array)
+  room_assignments = []
+  room_number = 1
+  until room_number == 8
+   array.each do |name|
+     room_assignments.push("Hello, #{name}! You'll be assigned to room #{room_number}!")
+     room_number= room_number+1
   end
- 
-     
+    break
+  end
+  room_assignments
+end
     
     
 
